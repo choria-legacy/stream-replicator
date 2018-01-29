f03c910867a1dac1344a5568e833d1f0becc9917
R.I.Pienaar <rip@devco.net>
2018-01-29 18:52:38 +0100

    (#65) puppet module incorrectly enumerates the advisory.cluster
    It allows cluster an source but should allow source and target
    as valid entries for the advisory target

--- puppet/types/advisory.pp
+++ puppet/types/advisory.pp
@@ -1,5 +1,5 @@
 type Stream_replicator::Advisory = Struct[{
     target => String,
-    cluster => Enum["cluster", "source"],
+    cluster => Enum["source", "target"],
     age => Pattern[/\d+(m|h)/]
 }]
